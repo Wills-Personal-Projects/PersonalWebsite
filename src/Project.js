24b08b7e5baee279c21402b39bc30c8ab416fa69
Will <wjett18@gmail.com>
2021-03-07 14:44:25 -0700

    added modals for art display and cleaned up project structure

--- src/Project.js
+++ src/Project.js
@@ -1,15 +1,73 @@
 import React from 'react';
-
+import {Header, Button, Modal, Icon} from 'semantic-ui-react'
+import fs from './images/fractalstickersmall.png'
+import lmo from './images/letmeoutsmall.png'
+import {Grid} from 'semantic-ui-react'
 import './App.css';
 
 function Project(props){
-    return(
-        <div>
-            <a href={props.githubUrl}>
-                <h4>{props.name}</h4>
-            </a>
-        </div>
-    );
+    const [open, setOpen] = React.useState(false)
+    if(!props.content){
+        return(
+            <div>
+                <a href={props.githubUrl}>
+                    <h4 style={{color:'#007DD0'}} >{props.name}</h4>
+                </a>
+            </div>
+        );
+    }else if(props.artName === 'sculpt'){
+        return (
+            <Modal
+              basic
+              onClose={() => setOpen(false)}
+              onOpen={() => setOpen(true)}
+              open={open}
+              size='small'
+              trigger={<h4 style={{color:'#007DD0'}}>{props.name}</h4>}
+            >
+              <Modal.Content>
+                <Grid>
+                    <Grid.Column>
+                        <Grid.Row>
+                            <img src={lmo}/>
+                        </Grid.Row>
+                    </Grid.Column>
+                </Grid>
+              </Modal.Content>
+              <Modal.Actions>
+                <Button basic color='red' inverted onClick={() => setOpen(false)}>
+                  <Icon name='remove' /> Close
+                </Button>
+              </Modal.Actions>
+            </Modal>
+          )
+    }else if(props.artName === 'sticker'){
+        return (
+            <Modal
+              basic
+              onClose={() => setOpen(false)}
+              onOpen={() => setOpen(true)}
+              open={open}
+              size='small'
+              trigger={<h4 style={{color:'#007DD0'}}>{props.name}</h4>}
+            >
+              <Modal.Content>
+                <Grid>
+                    <Grid.Column>
+                        <Grid.Row>
+                            <img src={fs}/>
+                        </Grid.Row>
+                    </Grid.Column>
+                </Grid>
+              </Modal.Content>
+              <Modal.Actions>
+                <Button basic color='red' inverted onClick={() => setOpen(false)}>
+                  <Icon name='remove' /> Close
+                </Button>
+              </Modal.Actions>
+            </Modal>
+          )
+    }
 }
 
 export default Project;
--- src/ProjectNavBar.js
+++ src/ProjectNavBar.js
@@ -16,25 +16,25 @@ export default class ProjectNavBar extends Component {
                                 <Grid.Column>
                                     <Menu style={{backgroundColor: 'lightgrey'}} fluid vertical>
                                         <Menu.Item>
-                                            <Project name='Nomadic Camping App' githubUrl={'https://nomadic1995.firebaseapp.com'} />
+                                            <Project name='Nomadic Camping App' content={false} githubUrl={'https://nomadic1995.firebaseapp.com'} />
                                         </Menu.Item>
                                         <Menu.Item>
-                                            <Project name='Nomadic Camping App Wiki' githubUrl={'https://nomadic-frontend-wiki-1995.web.app'} />
+                                            <Project name='Nomadic Camping App Wiki' content={false}  githubUrl={'https://nomadic-frontend-wiki-1995.web.app'} />
                                         </Menu.Item>
                                         <Menu.Item>
-                                            <Project name='Nomadic App Github' githubUrl={'https://github.com/Wills-Personal-Projects/NomadicReactApp'} />
+                                            <Project name='Nomadic App Github' content={false} githubUrl={'https://github.com/Wills-Personal-Projects/NomadicReactApp'} />
                                         </Menu.Item>
                                         <Menu.Item>
-                                            <Project name='Nomadic Backend Wiki' githubUrl={'https://nomadic-backend-wiki-1995.web.app'} />
+                                            <Project name='Nomadic Backend Wiki' content={false} githubUrl={'https://nomadic-backend-wiki-1995.web.app'} />
                                         </Menu.Item>
                                         <Menu.Item>
-                                            <Project name='Campsite Service Github' githubUrl={'https://github.com/Wills-Personal-Projects/campsiteService'} />
+                                            <Project name='Campsite Service Github'content={false} githubUrl={'https://github.com/Wills-Personal-Projects/campsiteService'} />
                                         </Menu.Item>
                                         <Menu.Item>
-                                            <Project name='This Website Wiki' githubUrl={'https://my-website-wiki-1995.web.app'} />
+                                            <Project name='This Website Wiki' content={false} githubUrl={'https://my-website-wiki-1995.web.app'} />
                                         </Menu.Item>
                                         <Menu.Item>
-                                            <Project name='This Website Github' githubUrl={'https://github.com/Wills-Personal-Projects/PersonalWebsite'} />
+                                            <Project name='This Website Github' content={false} githubUrl={'https://github.com/Wills-Personal-Projects/PersonalWebsite'} />
                                         </Menu.Item>
                                     </Menu>
                                 </Grid.Column>
@@ -51,10 +51,10 @@ export default class ProjectNavBar extends Component {
                                 <Grid.Column>
                                     <Menu style={{backgroundColor: 'lightgrey'}} fluid vertical>
                                         <Menu.Item>
-                                            <Project name='3D Sculptures' githubUrl={'https://google.com'} />
+                                            <Project name='3D Sculptures' content={true} artName={'sculpt'} />
                                         </Menu.Item>
                                         <Menu.Item>
-                                            <Project name='Sticker Designs' githubUrl={'https://google.com'} />
+                                            <Project name='Sticker Designs' content={true} artName={'sticker'} />
                                         </Menu.Item>
                                     </Menu>
                                 </Grid.Column>
